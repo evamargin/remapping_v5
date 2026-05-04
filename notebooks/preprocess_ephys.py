afad1c4fe226c0209c5ca18bc736d9d342bb4ebb
eva bukina <zhenya.bukina97@gmail.com>
2026-05-04 23:12:56 +0200

    preprocess_ephys: switch to evabox; playground: prototype load_binary

--- notebooks/preprocess_ephys.py
+++ notebooks/preprocess_ephys.py
@@ -1,6 +1,9 @@
+# %%
+%load_ext autoreload
+%autoreload 2
+
 # %% imports
 import numpy as np
-from pathlib import Path
 import pandas as pd
 from evabox import utils, ephys, plotting
 
@@ -11,3 +14,12 @@ params = utils.params_dict(animal, date)
 print(params)
 
 # %%
+ttl_type = params['ttl_type']
+paths = utils.build_paths(params['animal'], params['date'])
+# %%
+paths
+# %%
+ttl = ephys.load_binary(paths['raw_path'], channels=[384], n_channels=385)
+ttl.shape
+# %%
+# TODO: check if Lans preprocessed merging actually includes ttl channel!
